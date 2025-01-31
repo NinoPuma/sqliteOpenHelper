3f69f44df34df8af70b43c7bf2bbc7b02c857534
ELnino <apuma2002@gmail.com>
2025-01-31 20:42:34 +0100

    first commit

---
+++ app/src/main/java/com/example/slqliteopenhelper/AdminSQLiteOpenHelper.java
@@ -0,0 +1,37 @@
+package com.example.slqliteopenhelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, int version, @NonNull SQLiteDatabase.OpenParams openParams) {
+        super(context, name, version, openParams);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table LIDERESPOLITICOS(" + "NOMBRE TEXT PRIMARY KEY," + " EDAD INT," + "ESTUDIOS TEXT)");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+}
--- app/src/main/java/com/example/slqliteopenhelper/MainActivity.java
+++ app/src/main/java/com/example/slqliteopenhelper/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.slqliteopenhelper;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    AdminSQLiteOpenHelper gestor;
+    EditText etEstudios, etNombre, etEdad;
+    Button btn_agregar, btn_modificar, btn_borrar, btn_consultar, btn_5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        gestor = new AdminSQLiteOpenHelper(this, "politico", null, 1);
+    }
+    public void guardarPolitico(View view){
+        SQLiteDatabase db = gestor.getWritableDatabase();
+        ContentValues registro = new ContentValues();
+        registro.put("NOMBRE", etNombre.getText().toString());
+        registro.put("EDAD", Integer.parseInt(etEdad.getText().toString()));
+        registro.put("ESTUDIOS", etEstudios.getText().toString());
+        db.insert("LIDERESPOLITICOS", null, registro);
+        db.close();
+    }
+    public void consultarPolitica(View view) {
+        SQLiteDatabase db = gestor.getWritableDatabase();
+        String[] aux = {etNombre.getText().toString()};
+        Cursor fila = db.query("LIDERESPOLITICOS", null, "NOMBRE=?", aux, null, null, null);
+        if(fila.moveToFirst()){
+            etNombre.setText(fila.getString(0));
+        }
+    }
+    public void modificarPolitica(View view){
+        SQLiteDatabase db = gestor.getWritableDatabase();
+        ContentValues registro = new ContentValues();
+        registro.put("EDAD", Integer.parseInt(etEdad.getText().toString()));
+        registro.put("ESTUDIOS", etEstudios.getText().toString());
+        int filasUpdateadas = db.update("LIDERESPOLITICOS", registro, "NOMBRE=" + etNombre.getText().toString(), null);
+        if (filasUpdateadas == 1){
+            Toast.makeText(this, "Líder modificado", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Líder no encontrado", Toast.LENGTH_SHORT).show();
+        }
+        db.close();
+    }
+    public void borrarPolitico(View view){
+        SQLiteDatabase db = gestor.getWritableDatabase();
+        int filasBorradas = db.delete("LIDERESPOLITICOS", "NOMBRE=" + etNombre.getText().toString(), null);
+        if (filasBorradas == 1){
+            Toast.makeText(this, "Líder eliminado"+ etNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Líder no encontrado"+ etNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+        }
+        db.close();
     }
 }
